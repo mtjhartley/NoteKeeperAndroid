6d269ae1c12645bc642da7ca677c59e5399f8d13
Michael Hartley <mtjhartley@gmail.com>
2018-08-24 17:34:44 -0700

    changed notelistactivity to startup activity in android manifest, edited string resource files

--- app/src/main/java/com/mtjhartley/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/mtjhartley/notekeeper/NoteListActivity.java
@@ -1,11 +1,13 @@
 package com.mtjhartley.notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -41,6 +43,16 @@ public class NoteListActivity extends AppCompatActivity {
 
         listNotes.setAdapter(adapterNotes);
 
+        //create anonymous class here
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //activity interaction starts here
+                //specify notelistactivity.this because we're inside an anonymous class
+                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }

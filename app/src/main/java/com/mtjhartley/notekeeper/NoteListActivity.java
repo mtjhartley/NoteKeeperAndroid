9f8fe7be3f1a9dc08584029056e304eff7782dbf
Michael Hartley <mtjhartley@gmail.com>
2018-08-26 11:36:31 -0700

    fixed bug where new note would crash on back by implementing onResume and notifying arrayadapter dataset changed

--- app/src/main/java/com/mtjhartley/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/mtjhartley/notekeeper/NoteListActivity.java
@@ -14,6 +14,8 @@ import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
 
+    private ArrayAdapter<NoteInfo> mAdapterNotes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,14 +35,22 @@ public class NoteListActivity extends AppCompatActivity {
         initializeDisplayContent();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapterNotes.notifyDataSetChanged(); //build in method to the arrayadapter
+    }
+
+
+
     private void initializeDisplayContent() {
         //marking this as final allows us to use it in the anonymous class
         final ListView listNotes = findViewById(R.id.list_notes);
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
-        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+        mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
 
-        listNotes.setAdapter(adapterNotes);
+        listNotes.setAdapter(mAdapterNotes);
 
         //create anonymous class here
         listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {

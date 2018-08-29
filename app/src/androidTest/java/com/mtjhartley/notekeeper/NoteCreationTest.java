e16bfcb25df988e7e58c114f5c28ad33bd18e3e9
Michael Hartley <mtjhartley@gmail.com>
2018-08-28 18:21:27 -0700

    lay groundwork for instrumented testing

---
+++ app/src/androidTest/java/com/mtjhartley/notekeeper/NoteCreationTest.java
@@ -0,0 +1,21 @@
+package com.mtjhartley.notekeeper;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+@RunWith(AndroidJUnit4.class)
+public class NoteCreationTest {
+    @Rule
+    ActivityTestRule<NoteListActivity> mNoteListActivityRule =
+            new ActivityTestRule(NoteListActivity.class);
+
+    @Test
+    public void createNewNote() {
+
+    }
+}

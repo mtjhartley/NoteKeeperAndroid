2eb9508a1dd8aa21605a7d8e36b21fd820b20301
Michael Hartley <mtjhartley@gmail.com>
2018-08-30 19:55:15 -0700

    added settings to general with display name, email address and fav social

--- app/src/main/java/com/mtjhartley/notekeeper/MainActivity.java
+++ app/src/main/java/com/mtjhartley/notekeeper/MainActivity.java
@@ -2,6 +2,7 @@ package com.mtjhartley.notekeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -43,6 +44,9 @@ public class MainActivity extends AppCompatActivity
                 startActivity(new Intent(MainActivity.this, NoteActivity.class));
             }
         });
+        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_notification, false);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_data_sync, false);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(

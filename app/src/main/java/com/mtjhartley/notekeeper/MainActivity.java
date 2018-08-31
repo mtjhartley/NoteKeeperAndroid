f69446a4881713c7e98561447073afb23e6a55b6
Michael Hartley <mtjhartley@gmail.com>
2018-08-30 19:35:29 -0700

    added base settingsactivity and wired up to mainactivity

--- app/src/main/java/com/mtjhartley/notekeeper/MainActivity.java
+++ app/src/main/java/com/mtjhartley/notekeeper/MainActivity.java
@@ -2,6 +2,7 @@ package com.mtjhartley.notekeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.GridLayoutManager;
@@ -129,6 +130,7 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
